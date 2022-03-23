b7e6c22800963fe524ea2ccb0f25868c67cc9fb7
fadlirc676 <101616816+fadlirc676@users.noreply.github.com>
2022-03-23 19:46:21 +0700

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,57 @@
+/*NAMA KELOMPOK:1.AHMAD FADLI RISMANDA
+                2.REGITA NINA ANDANI
+                3.DWI PRIAMBODO PRAYOGA
+*/
+//BAGIAN fadli //
+#include <iostream>//file header
+#include <conio.h>//file header
+#include <fstream>//file header
+using namespace std;//perintah menggunaka standar libray
+float pertalite(float x){
+	float hasil;
+	return hasil = x * 7000 ;
+}
+float pertamax(float x){
+	float hasil;
+	return hasil = x * 9000;
+}
+int main(){
+	string nama,bensin;
+	int pil,hrg;
+	float liter,harga;
+//BAGIAN regita //
+	cout << "Program PERTAMINA Banguntapan\n";
+	cout << "Masukkan nama pembeli: "; cin  >> nama;
+	cout << "Jenis bensin :\n";
+	cout << "1.Pertalite \n2.Pertamax\n";
+	cout << "Masukkan pilihan jenis bensin : ";
+	cin >> pil;
+	if (pil == 1){
+		bensin = "Pertalite";
+		hrg = 7000;
+		cout << "Masukkan jumlah liter : " ;
+		cin >> liter;
+		harga = pertalite(liter);
+	}
+	else if (pil == 2){
+		bensin = "Pertamax";
+		hrg = 9000;
+		cout << "Masukkan jumlah liter : " ;
+		cin >> liter;
+		harga = pertamax(liter);
+	}
+// BAGIAN fadli //
+	cout << endl << endl<< endl;
+	cout<<" struk akan segera dicetak \n " ;\
+	ofstream spbu;
+	spbu.open("spbu.txt", ios::out);
+	spbu<<" STRUKTUR PEMBELIAN BENSIN\n";
+	spbu<<"==================================\n";
+	spbu<<" Nama Pembeli = "<<nama<<endl;
+	spbu<<" Jenis Bensin = "<<bensin<<endl;
+	spbu<<" Total Liter  = "<<liter<<endl;
+	spbu<<" Harga/liter  = "<<hrg<<endl;
+	spbu<<"==================================\n";
+	spbu<<"\n Total Pembelian : "<< harga;
+	spbu.close();
+}
